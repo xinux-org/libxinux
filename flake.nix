788fdc4c32599e2e6a7093edb05468c53b2bd2e7
bahrom04 <magdiyevbahrom@gmail.com>
2025-11-29 12:11:00 +0500

    chore: addded xinux-lib 25.11 and hydra

--- flake.nix
+++ flake.nix
@@ -3,20 +3,20 @@
 
   inputs = {
     # Latest nixpkgs from nixos
-    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-25.11";
 
     # Xinux Libs for flake automation
     xinux-lib = {
-      url = "github:xinux-org/lib";
+      url = "github:xinux-org/lib/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    inputs:
+  outputs = {self, ...}@inputs:
     inputs.xinux-lib.mkFlake {
       inherit inputs;
       alias.shells.default = "libxinux";
       src = ./.;
+      hydraJobs = inputs.self.packages.x86_64-linux;
     };
 }
